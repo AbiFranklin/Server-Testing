d57674bc2cb39946a0af25b833c1e27be8ee2d00
Abigayle Franklin <abifanklin@gmail.com>
2019-02-07 21:51:31 -0600

    Files in place to start writing api tests

---
+++ server.spec.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const server = require('./server.js');
+
+describe('testing api', () => {
+    describe('get /', () => {
+        
+    });
+
+    describe('post /', () => {
+        
+    })
+
+    describe('delete /', () => {
+        
+    })
+})
